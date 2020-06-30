41826a99a49f8040a3ba9b02b11cd9cc72445eeb
Fede Camara Halac <camarafede@gmail.com>
2020-06-30 16:53:26 -0400

    rearranged repo

--- app.sh
+++ app.sh
@@ -5,7 +5,7 @@ VERSION=1 # version number
 # exclude from the deploy
 EXCLUDE=(app app.sh backup backup.sh bak .gitignore)
 
-APP=/Applications/AccadAoo-${VERSION}.app # app bundle
+APP=AccadAoo-${VERSION}.app # app bundle
 
 PATCHDIR=${APP}/Contents/Resources/patch/ # 'patch' dir inside bundle
 
@@ -14,6 +14,8 @@ for i in ${EXCLUDE[@]}; do EX+="--exclude=$i "; done
 run() {
 	if [[ "$1" == "deploy" ]] || [[ $1 == "d" ]]; then
 		rsync -aPiv ${EX} ../bin/* ${PATCHDIR}
+		rm -rf ${APP}.zip
+		zip -r ${APP}.zip ${APP} setup.txt 
 	elif [[ "$1" == "backup" ]] || [[ $1 == "b" ]]; then
 		source ../backup.sh ${EX}
 	elif [[ "$1" == "test" ]] || [[ $1 == "t" ]]; then
@@ -34,7 +36,7 @@ else
 	echo "No arguments passed."
 	echo "--- Usage: ./app [t|d|b|r]"
 	printf "%6s\t(%s)%s\n"  "test"   "t" "ests the app bundle"
-	printf "%6s\t(%s)%s\n"  "deploy" "d" "eploys patch to app bundle"
+	printf "%6s\t(%s)%s\n"  "deploy" "d" "eploys & zips patch in app bundle"
 	printf "%6s\t(%s)%s\n"  "backup" "b" "ackups <bin> with backup.sh"
 	printf "%6s\t(%s)%s\n"  "run"    "r" "uns the pd file (development)"
 fi
